014959c31e29d559c73c52bf2e864393823a33b8
Kennedy Morganfield <kennedymorganfield@gmail.com>
2018-06-22 20:31:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,27 @@
+class Song
+  
+  attr_accessor :genre, :artist, :name
+  
+  def initialize(genre, artist, name)
+    @genre = genre
+    @artist = artist
+    @name = name
+  end
+  
+  # def gets_genre
+  #   @genre
+  # end
 
+  # def gets_artist
+  #   @artist
+  # end
+  
+  # def gets_name
+  #   @name
+  # end
+end
+
+song_1 = Song.new("Hip Hop", "Kendrick Lamar", "PRIDE.")
+puts song_1.genre
+puts song_1.artist
+puts song_1.name
